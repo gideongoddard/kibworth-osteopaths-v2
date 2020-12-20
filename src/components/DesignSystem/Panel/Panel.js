26fbb01e8a194cf3ceffdab445be87d441f05c59
Gideon Goddard <goddard.gcw@gmail.com>
2020-12-20 22:33:58 +0000

    Removed all files.

--- src/components/DesignSystem/Panel/Panel.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import PanelStyles from "./Panel.module.css"
-
-export default function Panel(props) {
-    return <div className={`${PanelStyles.panel} ${props.addedClass}`}>{props.children}</div>
-}
--- src/components/DesignSystem/TypeScale/TypeScale.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import TypeScaleStyles from "./TypeScale.module.css"
-
-export default function TypeScale(props) {
-    return (
-        <div className={`${TypeScaleStyles.type} ${props.addedClass}`}>The quick brown fox jumps over the lazy dog.</div>
-    )
-}
