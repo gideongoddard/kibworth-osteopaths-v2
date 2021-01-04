f58ea571fb531aa8052a048915b6525ddbfd351a
Gideon Goddard <goddard.gcw@gmail.com>
2021-01-04 10:23:05 +0000

    Updated class times in Schedule component.

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -12,15 +12,15 @@ export default function Schedule(props) {
                 props.type === "Pilates" ? (
                     <>
                         <div className={ScheduleStyles.dayTime}>
-                            <p className={ScheduleStyles.day}>Tuesdays:</p><p className={ScheduleStyles.time}>08:00 - 09:00 (The Fitness Box)</p>
-                            <p className={ScheduleStyles.day}>Wednesdays:</p><p className={ScheduleStyles.time}>13:45 - 15:45 (The Fitness Box)</p>
-                            <p className={ScheduleStyles.day}>Thursdays:</p><p className={ScheduleStyles.time}>18:00 - 19:00 & 19:15 - 20:15 (Mkt Harborough Congregational Church)</p>
+                            <p className={ScheduleStyles.day}>Tuesdays:</p><p className={ScheduleStyles.time}>08:00 - 09:00</p>
+                            <p className={ScheduleStyles.day}>Thursdays:</p><p className={ScheduleStyles.time}>18:00 - 19:00 & 19:15 - 20:15</p>
                         </div>
+                        <p>Pilates classes will be on Zoom until further notice.</p>
                         <p>Individual tuition is also available. Please get in touch by phone or email to make arrangements.</p>
                     </>
                 ) : (
                     <div className={ScheduleStyles.dayTime}>
-                        <p className={ScheduleStyles.day}>Mondays:</p><p className={ScheduleStyles.time}>08:00 - 14:00</p>
+                        <p className={ScheduleStyles.day}>Mondays:</p><p className={ScheduleStyles.time}>08:00 - 18:00</p>
                         <p className={ScheduleStyles.day}>Tuesdays:</p><p className={ScheduleStyles.time}>10:00 - 18:00</p>
                         <p className={ScheduleStyles.day}>Wednesdays:</p><p className={ScheduleStyles.time}>09:30 - 13:00</p>
                         <p className={ScheduleStyles.day}>Fridays:</p><p className={ScheduleStyles.time}>08:00 - 12:00</p>
