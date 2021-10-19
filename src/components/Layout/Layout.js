4be9b95d21bad3a225dc1ebe8c3f751f5f187ed7
Gideon Goddard <goddard.gcw@gmail.com>
2021-10-19 13:04:58 +0100

    Removed extra line from Coronavirus modal

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -26,7 +26,6 @@ class Layout extends React.Component {
                 <Modal heading="Coronavirus" closeModal={this.closeModal} displayModal={this.state.displayModal}>
                     <p>Hello and I hope you're well.</p>
                     <p>I wanted to offer you reassurance that the clinic is a safe place to come for treatment. Strict adherence to guidelines regarding PPE is adhered to by all members of staff as well as thorough cleaning of the surgery daily. We ask that patients wear a face covering unless in the case of medical exemption.</p>
-                    <p>For patients who have appointments booked, these will only be changed at your request.</p>
                     <p>Please contact me for any further information.</p>
                     <p>Emily <span role="img" aria-label="Open Hands Sign Emoji, representing openness.">🤲</span></p>
                 </Modal>
