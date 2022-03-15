7c0b7d6941e864f3475571176ca4a2de392be384
Gideon Goddard <goddard.gcw@gmail.com>
2022-03-15 22:57:57 +0000

    Added missing bracket in title component

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,7 @@ import Button from "../Button/Button"
 
 export default function Title(props) {
     return (
-        <div className={TitleStyles.container} style={{backgroundImage: props.backgroundImage ? `url(${props.backgroundImage})` : `none`, color: props.backgroundImage ? "white" : "var(--neutral-800", minHeight: props.backgroundImage ? 'calc(var(--space-12) + 15vw)' : 'calc(var(--space-12) + 3vw)', backgroundPosition: props.backgroundPosition, padding: props.backgroundImage ? "calc(6em + 5vw) 5%" : "calc(4em + 1.5vw) 5%"}}>
+        <div className={TitleStyles.container} style={{backgroundImage: props.backgroundImage ? `url(${props.backgroundImage})` : `none`, color: props.backgroundImage ? "white" : "var(--neutral-800)", minHeight: props.backgroundImage ? 'calc(var(--space-12) + 15vw)' : 'calc(var(--space-12) + 3vw)', backgroundPosition: props.backgroundPosition, padding: props.backgroundImage ? "calc(6em + 5vw) 5%" : "calc(4em + 1.5vw) 5%"}}>
             <h1>{props.title}</h1>
             {
                 props.subtitle ? (
