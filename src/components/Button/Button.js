06e5ecb6605e9d9cfc4c2ce824bb6f131571f54f
gideongoddard <goddard.gcw@gmail.com>
2023-06-21 00:03:12 +0100

    Removed prop-types import in button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import ButtonStyles from "./Button.module.css"
-import PropTypes from "prop-types"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDownload } from "@fortawesome/free-solid-svg-icons"
 
