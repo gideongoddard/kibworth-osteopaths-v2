521205dc5a658a0e046372a6e1c068cf87568e38
gideongoddard <goddard.gcw@gmail.com>
2023-07-20 22:15:12 +0100

    Updated spring 2023 newsletter for summer 2023 newsletter

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -32,7 +32,7 @@ const HubSpotForm = props => {
 function Newsletter() {
     const data = useStaticQuery(graphql`
         query {
-            file(name: {eq: "KOPI Spring 2023 Newsletter"}) {
+            file(name: {eq: "KOPI Summer 2023 Newsletter"}) {
                 publicURL
             }
         }
@@ -46,7 +46,7 @@ function Newsletter() {
                 <p>Subscribe to the Kibworth Osteopaths & Pilates newsletter. We'll only share what we think you'll find useful but if it isn't for you, you can unsubscribe at any time.</p>
                 <div className={NewsletterStyles.downloadContainer}>
                     <p style={{marginBottom: 0}}>If you missed out on our latest newsletter, you can get up to speed by downloading and reading it here.</p>
-                    <Button type="primary" contact={false} to={data.file.publicURL} download={true}>Spring 2023 newsletter</Button>
+                    <Button type="primary" contact={false} to={data.file.publicURL} download={true}>Summer 2023 newsletter</Button>
                 </div>
             </div>
             <HubSpotForm region="eu1" portalId="27185733" formId="ab6b5de1-c7bc-448d-8785-0550e4d4dbf6" />
